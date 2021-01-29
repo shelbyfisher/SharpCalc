5843dc4596c8d033a160d9c8e215d644227ec903
Berniemack <berniemack97@icloud.com>
2021-01-29 01:59:08 -0500

    Finished Prototype

--- SharpCalc/Program.cs
+++ SharpCalc/Program.cs
@@ -6,7 +6,24 @@ namespace SharpCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                DataConverter dataConverter = new DataConverter();
+                TheCalc theCalc = new TheCalc();
+
+                double firstNumber = dataConverter.ConvertToNumeric(Console.ReadLine());
+                double secondNumber = dataConverter.ConvertToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = theCalc.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
---
+++ SharpCalc/TheCalc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpCalc
+{
+    class TheCalc
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+
+            double result;
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Not Valid");
+
+            }
+
+            return result;
+
+
+        }
+    }
+}
